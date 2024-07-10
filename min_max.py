4ab912de426a161b91f61d24c5118c27b3150f4e
trisselj <trisselj@oregonstate.edu>
2024-07-10 14:36:58 -0700

    nesting error

--- min_max.py
+++ min_max.py
@@ -6,22 +6,22 @@
 #Initial question for integers that also makes input read as integer
 integers = int(input("How many integers would you like to enter?"))
 
-#Secondary list of integer questions to ask for inputs
-first_integer = int(input(f"Please enter your {integers} integers."))
-min = first_integer
-max = first_integer
+if integers >= 1:
+    #Secondary list of integer questions to ask for inputs
+    first_integer = int(input(f"Please enter your {integers} integers."))
+    min = first_integer
+    max = first_integer
 
-#Loop for the rest of the integers
-if integers > 1:
+    #Loop for the rest of the integers
     for _ in range(integers - 1):
         current_integer = int(input()) 
 
-#Updating the min and max value as the integers are entered
-    if current_integer < min:
-        min = current_integer
-    if current_integer > max:
-        max = current_integer
+    #Updating the min and max value as the integers are entered
+        if current_integer < min:
+            min = current_integer
+        if current_integer > max:
+            max = current_integer
 
-#Printing of final min and max values
-print(f"min: {min}")
-print(f"max: {max}")
+    #Printing of final min and max values
+    print(f"min: {min}")
+    print(f"max: {max}")
